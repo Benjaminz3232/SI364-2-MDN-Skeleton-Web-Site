c3e8c8b46e7d121492da5d8ffad34870d338b97d
Benjamin Zeffer <bzeffer@umich.edu>
2019-02-14 04:49:00 +0000

    attempting to fix issues

--- catalog/admin.py
+++ catalog/admin.py
@@ -33,9 +33,9 @@ class BookInstanceAdmin(admin.ModelAdmin):
 
     fieldsets = (
         (None, {
-            'fields': ('book', 'imprint', 'id')
+            'fields': ('book','imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back', 'borrower')
+            'fields': ('status', 'due_back','borrower')
         }),
     )
--- catalog/views.py
+++ catalog/views.py
@@ -46,7 +46,7 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
     template_name ='catalog/bookinstance_list_borrowed_user.html'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
